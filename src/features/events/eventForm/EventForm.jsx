e0051ddbd5897ab07964bd28ab827e81b1786f97
Joshua Rose <jlr007@protonmail.com>
2021-11-13 22:19:16 -0500

    End of section 5

--- src/features/events/eventForm/EventForm.jsx
+++ src/features/events/eventForm/EventForm.jsx
@@ -1,5 +1,6 @@
 import cuid from 'cuid';
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Button, Form, Header, Segment } from 'semantic-ui-react';
 
 export default function EventForm({
@@ -97,6 +98,7 @@ export default function EventForm({
         </Form.Field>
         <Button type='submit' floated='right' positive content='Submit' />
         <Button
+        as={Link} to='/events'
           onClick={() => setFormOpen(false)}
           type='submit'
           floated='right'
--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'semantic-ui-css/semantic.min.css'
+import 'semantic-ui-css/semantic.min.css';
 import './app/layout/styles.css';
 import App from './app/layout/App.jsx';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 const rootEl = document.getElementById('root');
 
 function render() {
-  ReactDOM.render(<App />, rootEl)
+  ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    rootEl
+  );
 }
 
 if (module.hot) {
-  module.hot.accept('./app/layout/App', function() {
+  module.hot.accept('./app/layout/App', function () {
     setTimeout(render);
-  })
+  });
 }
 
 render();
